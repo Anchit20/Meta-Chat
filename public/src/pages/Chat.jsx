73bcffa66939d36a5f0288e0c661c99fe4be0ad3
Anchit20 <anchitgedekar@gmail.com>
2024-04-05 09:20:26 +0530

    session-logout

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -5,12 +5,15 @@ import { useNavigate } from "react-router-dom";
 import { allUsersRoute } from "../utils/APIRoutes";
 import Contacts from "../components/Contacts";
 import Welcome from "../components/Welcome";
+import ChatContainer from "../components/ChatContainer";
 
 function Chat() {
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
   const [currentChat, setCurrentChat] = useState(undefined);
+  const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
     const getUser = async () => {
       if (!localStorage.getItem("chat-app-user")) {
@@ -18,6 +21,7 @@ function Chat() {
       } else {
         setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
         // console.log("asjdas");
+        setIsLoaded(true);
       }
     };
     getUser();
@@ -51,7 +55,11 @@ function Chat() {
           currentUser={currentUser}
           changeChat={handleChatChange}
         />
-        <Welcome currentUser={currentUser} />
+        {isLoaded && currentChat === undefined ? (
+          <Welcome currentUser={currentUser} />
+        ) : (
+          <ChatContainer currentChat={currentChat} />
+        )}
       </div>
     </Container>
   );
